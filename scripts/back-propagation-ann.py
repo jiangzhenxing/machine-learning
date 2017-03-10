269b3c3cdce13c45cfe4e98679fdf55786594dff
jiangzhenxing <107x@163.com>
2017-03-10 15:49:16 +0800

    update

--- scripts/back-propagation-ann.py
+++ scripts/back-propagation-ann.py
@@ -1,201 +1,57 @@
+#!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt 
 import logging
+import ml.ann.back_propagation_ann as bnn
 
 from functools import reduce
 from operator import add
 from operator import mul
+from ml.ann.back_propagation_ann import simple_back_propagation_ann
+from ml.util import scatter_datas
 
 '''
 使用反向传播算法训练前馈神经网络
 '''
 
-e = np.e
-ln = np.log
-extend = lambda x: np.array([1] + x)        # 扩展样本数据，增加常数项
-model = lambda x: np.sqrt(x.dot(x))         # 向量模长
-logistic = lambda x: 1 / (1 + e ** -x)      # 逻辑函数
+extend = bnn.extend        # 扩展样本数据，增加常数项
 classify = lambda x, threshold, c1, c2: c1 if x >= threshold else c2    # 分类函数
-SIGMOID = 'sigmoid'
-LINEAR = 'linear'
-S = SIGMOID
-L = LINEAR
+SIGMOID = bnn.SIGMOID
+LINEAR = bnn.LINEAR
 
 logging.basicConfig(level=logging.INFO, 
                     format='[%(asctime)s line:%(lineno)d %(levelname)s] %(message)s',
                     datefmt='%H:%M:%S')
 
-class Node:
-    '''
-    神经网络的结点
-    包括权重向量，输出函数，导数，陨失函数及其导数
-    '''
-    def __init__(self, weight, index, type=SIGMOID):
-        self.weight = weight
-        self.index = index
-        self.type=type
-        if type == SIGMOID:
-            # 输出函数
-            self.output = lambda x: logistic(self.weight.dot(x))
-            # 输出关于输入的导数
-            self.derivative = lambda x: self.output(x) * (1 - self.output(x)) * self.weight
-            # 输出关于权重向量的导数
-            self.derivative_w = lambda x: (self.output(x) * (1 - self.output(x))) * x
-            # 陨失函数
-            self.cost = lambda x,t: self.output(x) if t == 1 else 1 - self.output(x)
-            # 损失关于输入的导数
-            # self.cost_derivative = lambda o,t: t * (1 - o) - (1 - t) * o
-            self.cost_derivative = lambda x,t: (t * (1 - self.output(x)) - (1 - t) * self.output(x)) * self.weight
-            # 损失关于权重的导数
-            self.cost_derivative_w = lambda x,t: (t * (1 - self.output(x)) - (1 - t) * self.output(x)) * x
-        elif type == LINEAR:
-            # 输出函数
-            self.output = lambda x: self.weight.dot(x)
-            # 输出关于输入的导数
-            self.derivative = lambda x: self.weight
-            # 输出关于权重向量的导数
-            self.derivative_w = lambda x: x
-            # 陨失函数
-            self.cost = lambda o,t: 0.5 * (o - t) ** 2
-            # self.cost_derivative = lambda o,t: o - t
-            # 损失关于输入的导数
-            self.cost_derivative = lambda x,t: (self.output(x) - t) * self.weight
-            # 损失关于权重的导数
-            self.cost_derivative_w = lambda x,t: (self.output(x) - t) * x
-
-    def update_weight(self, delta):
-        self.weight += delta
-
-def simple_back_propagation_ann(datas, num_output=3, num_hidden=5, output_type=LINEAR, iterations=100, step=1):
-    '''
-    一个使用反向传播算法训练的简单的前馈神经网络
-    包含一个输出层，一个隐藏层和一个输入层
-    num_output: 输出结点的数量
-    num_hidden: 隐藏结点的数量
-    隐藏结点为sigmoid结点
-    输出结点为普通线性输出单元: out = wx
-    误差函数为LMS函数: 0.5 * ∑ (o - t) ** 2
-    '''
-    # 输出结点
-    outputs = [ Node(weight=np.random.rand(num_hidden + 1), index=i, type=output_type) for i in range(num_output) ]
-    # 隐藏结点
-    hidden_type = SIGMOID
-    hiddens = [ Node(weight=np.random.rand(len(datas[0]) + 1), index=i, type=hidden_type) for i in range(num_hidden) ]
-
-    # 权重更新的方向
-    # 如果输出结点是线性单元，取梯度向量相反的方向为误差下降最快方向
-    # 如果输出单元是sigmoid单元，取梯度向量方向为目标值上升最快方向
-    update_direct = 1 if output_type == SIGMOID else -1
-    step_delta = step / (iterations + 1)
-    for i in range(iterations):
-        #step0 = step / (i + 1)
-        step = step - step_delta
-        for x,t in datas:
-            x = extend(x)
-            logging.debug('x is: ' + str(x) + ', t is: ' + str(t))
-
-            # 计算隐藏结点的输出
-            out_hiddens = extend([ node.output(x) for node in hiddens ])
-            logging.debug('out_hiddens: ' + str(out_hiddens))
-
-            # 计算输出结点的输出
-            outs = [ node.output(out_hiddens) for node in outputs ]
-            logging.debug('outs: ' + str(outs))
-
-            # 计算输出单元损失的梯度向量
-            gradient_outputs = [ node.cost_derivative_w(out_hiddens, t) for node in outputs ]
-            logging.debug('gradient_outputs: ' + str(gradient_outputs))
-
-            # 输出结点损失函数关于隐藏结点输出的导数
-            d_cost_hidden = [ node.cost_derivative(out_hiddens, t)[1:] for node in outputs ]
-            logging.debug('d_cost_hidden: ' + str(d_cost_hidden))
-
-            # 隐藏结点关于输入权重的导数
-            d_hidden_weight = [ node.derivative_w(x) for node in hiddens ]
-            logging.debug('d_hidden_weight: ' + str(d_hidden_weight))
-
-            # 陨失函数(各输出结点关于某隐藏结点陨失之和)关于输入权重的导数(与该隐藏结点关于输入权重的导数相乘)
-            gradient_hiddens = list(map(mul, reduce(add, d_cost_hidden), d_hidden_weight))
-            logging.debug('gradient_hiddens: ' + str(gradient_hiddens))
-
-            # 更新权值向量
-            # LMS误差度量方式时，方向取梯度向量的反方向即误差下降最快方向
-            # 输出单元为sigmoid单元时，方向取梯度向量方向即可，为目标上升最快方向
-            foreach(Node.update_weight, outputs, update_direct * step * np.array(gradient_outputs))
-            foreach(Node.update_weight, hiddens, update_direct * step * np.array(gradient_hiddens))
-
-            '''
-            for gradient, node in zip(gradient_outputs, outputs):
-                node.update_weight(step0 * -gradient)
-
-            for gradient, node in zip(gradient_hiddens, hiddens):
-                node.update_weight(step0 * -gradient)
-            '''
-
-        logging.info('step is: ' + str(step))
-        logging.info('out weight is: ' + str([ node.weight for node in outputs ]))
-        logging.info('hidden weight is: ' + str([ node.weight for node in hiddens ]))
-        logging.info('=' * 100)
-
-    # 神经网络的输出函数
-    def output(x):
-        x = extend(x)
-        out_hiddens = extend([ node.output(x) for node in hiddens ])
-        outs = [ node.output(out_hiddens) for node in outputs ]
-        return outs
-
-    return output
-
-
-
-# 对多个输出结点的输出进行汇总输出
-def classify_outputs(outs):
-    return classify(sum(outs) / len(outs))
-
-def foreach(function, *iterators):
-    for args in zip(*iterators):
-        function(*args)
-
-def scatter_datas(datas, c1=1, c2=-1):
-    # 绘制原始数据点
-    data1 = [ d[0] for d in datas if d[1] == c1 ]
-    data2 = [ d[0] for d in datas if d[1] == c2 ]
-
-    if len(data1) > 0:
-        plt.scatter(*list(zip(*data1)), s=25, c='g', alpha=0.5)
-
-    if len(data2) > 0:
-        plt.scatter(*list(zip(*data2)), s=25, c='r', alpha=0.5)
-
-
-output_type = SIGMOID # LINEAR
-output_type = SIGMOID
+output_type = LINEAR
+# output_type = SIGMOID
 c1 = 1
 c2 = 0 if output_type == SIGMOID else -1
 threshold = 0 if output_type == LINEAR else 0.5
 
+t1 = [c1]
+t2 = [c2]
 # 一个以直线 y=x 为界的线性可分的数据集
-datas1 = [ ([x,y],c1) if x>y else ([x,y],c2) for x in range(10) for y in range(10) if x!=y ]
+datas1 = [ ([x,y],t1) if x>y else ([x,y],t2) for x in range(10) for y in range(10) if x!=y ]
 
 # 一个线性不可分的数据集，大至以y=x分隔，在直线y=x附近有线性不可分的点
-datas2 = [ ([x,y],c1) if x>y else ([x,y],c2) for x in range(10) for y in range(10) if np.abs(x - y) > 1 ]
-datas2.extend([([9,8],c2), ([8,9],c1), ([8,7],c1), ([7,8],c2), ([7,6],c2), ([6,7],c1), ([6,5],c1), ([5,6],c2), \
-               ([5,4],c2), ([4,5],c1), ([4,3],c1), ([3,4],c2), ([3,2],c2), ([2,3],c1), ([2,1],c1), ([1,2],c2), \
-               ([1,0],c2), ([0,1],c1), ])
+datas2 = [ ([x,y],t1) if x>y else ([x,y],t2) for x in range(10) for y in range(10) if np.abs(x - y) > 1 ]
+datas2.extend([([9,8],t2), ([8,9],t1), ([8,7],t1), ([7,8],t2), ([7,6],t2), ([6,7],t1), ([6,5],t1), ([5,6],t2), \
+               ([5,4],t2), ([4,5],t1), ([4,3],t1), ([3,4],t2), ([3,2],t2), ([2,3],t1), ([2,1],t1), ([1,2],t2), \
+               ([1,0],t2), ([0,1],t1), ])
 # 一个以半径为8的1/4圆弧为分界的数据集
-datas3 = [ ([x,y],c1) if x ** 2 + y ** 2 < 64 else ([x,y],c2) for x in range(10) for y in range(10) ]
+datas3 = [ ([x,y],t1) if x ** 2 + y ** 2 < 64 else ([x,y],t2) for x in range(10) for y in range(10) ]
 
 datas = np.array(datas2)                    # 选择数据集
-# scatter_datas(datas, c1, c2)
+# scatter_datas(plt, datas, c1, c2)
 
-# 完全拟合data3数据集
-# ann = simple_back_propagation_ann(datas, num_output=2, num_hidden=15, output_type=output_type, iterations=200, step=0.3)
-ann = simple_back_propagation_ann(datas, num_output=1, num_hidden=35, output_type=output_type, iterations=200, step=0.5)
+# ann = simple_back_propagation_ann(datas, num_output=1, num_hidden=15, output_type=output_type, iterations=200, step=0.3)
+ann = simple_back_propagation_ann(datas, num_output=1, num_hidden=15, output_type=output_type, iterations=100, step=0.5)
 results = [ (ann(x),t) for x,t in datas ]
 logging.info('results is: ')
 logging.info(results)
 
-classifys = [ (classify(sum(out)/len(out), threshold, c1, c2),t) for out,t in results ]
+classifys = [ ([classify(o, threshold, c1, c2) for o in out ], t) for out,t in results ]
 logging.info('分类结果为: ')
 logging.info(classifys)
 
@@ -209,7 +65,7 @@ logging.info('error rate is: ' + str(error_rate))
 
 # 误分类的数据
 error_classify_datas = datas[[ i for i,(c,t) in enumerate(classifys) if c != t ]]
-scatter_datas(error_classify_datas, c1, c2)
+scatter_datas(plt, error_classify_datas, t1, t2)
 
 plt.show()
 
--- scripts/perceptron.py
+++ scripts/perceptron.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
 from functools import reduce
+from ml.ann.perceptron import perceptron_rule,extend
+from ml.util import scatter_datas
 
 '''
 训练一个线性函数对数据集进行划分
@@ -13,125 +16,8 @@ from functools import reduce
 LMS最小均方差: ∑(ti-wxi)**2，是一个比较常用的误差度量方式，最优即使数据集方差最小
 LOGISTIC逻辑函数，最优即使样本相应类别逻辑概率的乘积最大
 '''
-e = np.e
-ln = np.log
-extend = lambda x: np.array([1] + x)    # 扩展样本数据，增加常数项
-model = lambda x: np.sqrt(x.dot(x))	# 向量模长
-logistic = lambda x: 1 / (1 + e ** -x)	# 逻辑函数
+
 classify = lambda x: 1 if x >= 0 else -1    # 分类函数
-
-def perceptron_rule(datas, iterations=100, step=0.1):
-    '''
-    感知器法则
-    对于一个线性可分的数据集: {(x1,t1),(x2,t2), ... ,(xn,tn)}
-    oi = sign(wxi)
-    ∆w = λ(ti - oi)xi
-    w = w + ∆w
-    循环这个过程至所有数据均正确分类，w不再改变，过程收敛
-    '''
-    w = np.random.rand(3)
-    print('init w is: ' + str(w))
-    for i in range(iterations):
-        step = 1 / (i + 1)
-        raw_w = np.array(w)
-        for x,t in datas:
-            x = np.array([1] + x)
-            o = np.sign(w.dot(x))
-            dw = (t - o) * x
-            w = w + step * dw
-        print('w is: ' + str(w))
-        if (w == raw_w).all():
-            break
-    return w
-
-def grad_desent_regression(datas, iteration=100, step=1, initw=0, cost='LMS'):
-    '''
-    对于一个线性不可分的数据集: {(x1,t1),(x2,t2), ... ,(xn,tn)}
-    找到一个线性函数，使得这个线性函数对整个数据集划分的误差最小
-    cost: 误差度量方式
-    LMS指最小均方差: ∑(ti-oi)**2，是一个比较常用的误差度量方式
-    对数据集中的每个样本xi:
-    oi = wxi
-    cost = ∑(ti-oi)**2 = ∑(ti-wxi)**2
-    ▽ w = ∑2(ti-wxi)xi
-    w = w + λ▽ w
-    重复这个过程至w收敛
-    LOGISTIC指逻辑函数，即使样本相应类别逻辑概率的乘积最大
-    Pi = 1 / (1 + e**(-wxi))
-    cost = ln(∏Pi**ti(1-Pi)**(1-ti)) = ∑ln(Pi**ti(1-Pi)**(1-ti)) = -∑[tiln(1+e**(-wxi)) + (1-ti)ln(1+e**wxi)]
-    ▽ w = ∑[ti/(1+e**wxi) - (1-ti)/(1+e**(-wxi))]xi
-    '''
-    if initw == 0: w = np.zeros(len(datas[0]) + 1)
-    elif initw == 'random': w = np.random.rand(len(datas[0]) + 1)
-    else: w = np.array(initw)
-    print('init w is ' + str(w))
-
-    for i in range(iteration):
-        delta = np.zeros(len(datas[0]) + 1)
-        step0 = step / (i * 5 + 1)
-        for x,t in datas:
-            x = np.array([1] + x)
-            # print('x is: ' + str(x) + ', w is: ' + str(w))
-            if cost == 'LMS':
-                delta += (w.dot(x) - t) * x
-            elif cost == 'LOGISTIC':
-                if t == -1: t = 0
-                delta += (t / (1 + e ** w.dot(x)) - (1 - t) / (1 + e ** -w.dot(x))) * x
-        if model(delta) < 1e-20: continue
-        gradient = delta
-
-        if cost == 'LMS':
-            w = w - step0 * gradient # 梯度向量方向为误差上升最快方向，故取负为误差下降最快方向
-            error = sum([ w.dot(np.array([1] + x)) - t for x,t in datas ])
-        elif cost == 'LOGISTIC':
-            w = w + step0 * gradient # 梯度向量方向为样本对应类别概率乘积上升最快方向
-            #error = -sum([ ln(1 + e ** -w.dot(np.array([1] + x))) if t == 1 else ln(1 + e ** w.dot(np.array([1] + x))) for x,t in datas ])
-            error = reduce(lambda x,y: x * y, [ 1 / (1 + e ** -w.dot(np.array([1] + x))) if t == 1 else 1 / (1 + e ** w.dot(np.array([1] + x))) for x,t in datas ])
-        print('step0 is ' + str(step0) + ', delta is ' + str(delta) + ', w is ' + str(w) + \
-                ', slope is ' + str(- w[1] / w[2]) + ', error is ' + str(error))
-
-    return w
-
-def stoch_grad_desent_regression(datas, iteration=100, step=1, initw=0, cost='LMS'):
-    '''
-    随机梯度下降与梯度下降相似，
-    只是对数据集中的每个样本xi，梯度向量的方向取(ti-wxi)xi，然后根据此梯度向量的方向更新w，不需要根据整个数据集样本的误差计算梯度向量
-    这相当于为每个样本定义一个误差函数：cost = (ti - ti) ** 2 = (ti - wxi) ** 2 和梯度向量(ti - wxi)xi
-    逻辑回归中目标函数为：-tiln(1 + e ** -wxi) - (1 - ti)ln(1 + e ** wxi)
-    梯度向量为：(ti / (1 + e ** wxi) - (1 - ti) / (1 + e ** -wxi))xi
-    '''
-    if initw == 0: w = np.zeros(len(datas[0]) + 1)
-    elif initw == 'random': w = np.random.rand(len(datas[0]) + 1)
-    else: w = np.array(initw)
-    print('init w is ' + str(w))
-
-    for i in range(iteration):
-        step0 = step / (i * 2 + 1)
-        for x,t in datas:
-            x = extend(x)
-            if cost == 'LMS':
-                delta = (w.dot(x) - t) * x
-                w = w - step0 * delta # 梯度向量方向为误差上升最快方向，故取负为误差下降最快方向
-            elif cost == 'LOGISTIC':
-                delta = (1 / (1 + e ** w.dot(x)) if t == 1 else -1 / (1 + e ** -w.dot(x))) * x
-                w = w + step0 * delta # 梯度向量方向为概率乘积上升最快方向
-        if cost == 'LMS':
-            error = sum([ 0.5 * (w.dot(extend(x)) - t) ** 2 for x,t in datas ])
-        elif cost == 'LOGISTIC':
-            error = sum([ ln(logistic(w.dot(extend(x)))) if t == 1 else ln(1 - logistic(w.dot(extend(x)))) for x,t in datas ])
-            #error = reduce(lambda x,y: x * y, [ logistic(w.dot(extend(x))) if t == 1 else 1 - logistic(w.dot(extend(x))) for x,t in datas ])
-        print('step0 is ' + str(step0) + ', delta is ' + str(delta) + ', w is ' + str(w) + \
-                ', slope is ' + str(- w[1] / w[2]) + ', error is ' + str(error))
-        
-    return w
-
-def scatter_datas(datas):
-    # 绘制原始数据点
-    data1 = [ d[0] for d in datas if d[1] == 1 ] 
-    data2 = [ d[0] for d in datas if d[1] == -1 ]
-    
-    plt.scatter(*list(zip(*data1)), s=25, c='g', alpha=0.5)
-    plt.scatter(*list(zip(*data2)), s=25, c='r', alpha=0.5)
 
 # 一个以直线 y=x 为界的线性可分的数据集
 datas1 = [ ([x,y],1) if x>y else ([x,y],-1) for x in range(10) for y in range(10) if x!=y ]
@@ -145,7 +31,7 @@ datas = datas1                  # 在这里选择使用的数据集
 w = perceptron_rule(datas)     # 这个方法要求使用线性可分的数据集
 #w = grad_desent_regression(datas, iteration=10, step=0.1, cost='LOGISTIC')
 #w = stoch_grad_desent_regression(datas, step=0.02, iteration=100, initw=0, cost='LMS')
-scatter_datas(datas)
+scatter_datas(plt, datas)
 print('w is: ' + str(w))
 
 # 绘制线性回归的直线
